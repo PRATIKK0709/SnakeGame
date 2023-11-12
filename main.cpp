6264e32a48332592cdda28907afc682810562da0
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2023-11-12 15:39:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <deque>
-#include <cstdlib> // for rand function
 
-// Define the size of the grid and the size of each cell
 const int gridSize = 20;
 const int cellSize = 20;
 
-// Define the direction enum
 enum class Direction { Up, Down, Left, Right };
+bool inGame = false;
+bool inGameOverScreen = false;
+float gameOverScreenAlpha = 0.0f;
+const float fadeSpeed = 150.0f; // Adjust the fade-in speed
 
-// Define the Snake class
 class Snake {
 public:
     Snake(int initialLength);
@@ -26,12 +26,10 @@ private:
     Direction direction;
     bool hasEaten;
 
-    // Helper method to add a new body segment without removing the tail
     void addBodySegment(sf::Vector2i position);
 };
 
 Snake::Snake(int initialLength) : hasEaten(false) {
-    // Initialize the snake with a starting length and direction
     for (int i = 0; i < initialLength; ++i) {
         body.push_front({i, 0});
     }
@@ -39,7 +37,6 @@ Snake::Snake(int initialLength) : hasEaten(false) {
 }
 
 void Snake::move() {
-    // Move the snake in the current direction
     sf::Vector2i head = body.front();
     switch (direction) {
         case Direction::Up:
@@ -56,35 +53,28 @@ void Snake::move() {
             break;
     }
 
-    // Check if the snake has eaten
     if (hasEaten) {
         hasEaten = false;
     } else {
-        // If not, remove the tail
         body.pop_back();
     }
 }
 
 void Snake::grow() {
-    // Set a flag to indicate that the snake has eaten
     hasEaten = true;
 }
 
 void Snake::addBodySegment(sf::Vector2i position) {
-    // Add a new body segment to the snake without removing the tail
     body.push_front(position);
 }
 
 bool Snake::checkCollision() {
-    // Check if the snake has collided with the window boundaries or itself
     sf::Vector2i head = body.front();
 
-    // Check if the head is outside the window boundaries
     if (head.x < 0 || head.x >= gridSize || head.y < 0 || head.y >= gridSize) {
         return true;
     }
 
-    // Check if the snake has collided with itself
     for (auto it = body.begin() + 1; it != body.end(); ++it) {
         if (*it == head) {
             return true;
@@ -95,7 +85,6 @@ bool Snake::checkCollision() {
 }
 
 void Snake::setDirection(Direction dir) {
-    // Set the direction, ensuring that the snake cannot reverse direction instantly
     if ((dir == Direction::Up && direction != Direction::Down) ||
         (dir == Direction::Down && direction != Direction::Up) ||
         (dir == Direction::Left && direction != Direction::Right) ||
@@ -105,7 +94,6 @@ void Snake::setDirection(Direction dir) {
 }
 
 void Snake::draw(sf::RenderWindow& window) {
-    // Draw each segment of the snake
     for (const sf::Vector2i& segment : body) {
         sf::RectangleShape bodySegment(sf::Vector2f(cellSize, cellSize));
         bodySegment.setPosition(segment.x * cellSize, segment.y * cellSize);
@@ -115,11 +103,9 @@ void Snake::draw(sf::RenderWindow& window) {
 }
 
 sf::Vector2i Snake::getHeadPosition() {
-    // Get the position of the snake's head
     return body.front();
 }
 
-// Define the Food class
 class Food {
 public:
     Food();
@@ -132,17 +118,14 @@ private:
 };
 
 Food::Food() {
-    // Set the initial position of the food
     respawn();
 }
 
 void Food::respawn() {
-    // Respawn the food in a random position within the grid
     position = {rand() % gridSize, rand() % gridSize};
 }
 
 void Food::draw(sf::RenderWindow& window) {
-    // Draw the food at its position
     sf::RectangleShape food(sf::Vector2f(cellSize, cellSize));
     food.setPosition(position.x * cellSize, position.y * cellSize);
     food.setFillColor(sf::Color::Red);
@@ -150,81 +133,240 @@ void Food::draw(sf::RenderWindow& window) {
 }
 
 sf::Vector2i Food::getPosition() {
-    // Get the position of the food
     return position;
 }
 
-// Define the main function
+class Menu {
+public:
+    Menu(sf::RenderWindow& window);
+    void draw();
+    void update(sf::Event& event);
+    int getSelectedItemIndex();
+    sf::Font& getFont(); // Added function to get the font
+
+private:
+    sf::RenderWindow& window;
+    sf::Font font;
+    sf::Text title;
+    sf::Text startText;
+    sf::Text exitText;
+    int selectedItemIndex;
+
+    void updateTextAppearance();
+};
+
+Menu::Menu(sf::RenderWindow& window) : window(window), selectedItemIndex(0) {
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cerr << "Error: Unable to load font 'arial.ttf'" << std::endl;
+    }
+
+    title.setFont(font);
+    title.setString("SFML Snake Game");
+    title.setCharacterSize(35);  // Set a smaller font size
+    title.setFillColor(sf::Color::White);
+
+    startText.setFont(font);
+    startText.setString("Start Game");
+    startText.setCharacterSize(25);  // Set a smaller font size
+    startText.setFillColor(sf::Color::White);
+
+    exitText.setFont(font);
+    exitText.setString("Exit");
+    exitText.setCharacterSize(25);  // Set a smaller font size
+    exitText.setFillColor(sf::Color::White);
+
+    updateTextAppearance();
+}
+
+void Menu::draw() {
+    sf::RectangleShape menuBackground(sf::Vector2f(window.getSize().x / 2, window.getSize().y));
+    menuBackground.setFillColor(sf::Color(0, 0, 0, 150));
+    window.draw(menuBackground);
+
+    window.draw(title);
+    window.draw(startText);
+    window.draw(exitText);
+}
+
+void Menu::update(sf::Event& event) {
+    switch (event.key.code) {
+        case sf::Keyboard::Up:
+            if (selectedItemIndex > 0) {
+                selectedItemIndex--;
+                updateTextAppearance();
+            }
+            break;
+
+        case sf::Keyboard::Down:
+            if (selectedItemIndex < 1) {
+                selectedItemIndex++;
+                updateTextAppearance();
+            }
+            break;
+
+        case sf::Keyboard::Enter:
+            if (selectedItemIndex == 0) {
+                inGame = true;
+                inGameOverScreen = false;
+            } else if (selectedItemIndex == 1) {
+                window.close();
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+int Menu::getSelectedItemIndex() {
+    return selectedItemIndex;
+}
+
+sf::Font& Menu::getFont() {
+    return font;
+}
+
+void Menu::updateTextAppearance() {
+    switch (selectedItemIndex) {
+        case 0: 
+            startText.setFillColor(sf::Color::Yellow);
+            exitText.setFillColor(sf::Color::White);
+            break;
+
+        case 1:
+            startText.setFillColor(sf::Color::White);
+            exitText.setFillColor(sf::Color::Yellow);
+            break;
+
+        default:
+            break;
+    }
+
+    title.setPosition(window.getSize().x / 2 - title.getGlobalBounds().width / 2, window.getSize().y / 6);
+    startText.setPosition(window.getSize().x / 2 - startText.getGlobalBounds().width / 2, window.getSize().y / 2);
+    exitText.setPosition(window.getSize().x / 2 - exitText.getGlobalBounds().width / 2, window.getSize().y / 1.5);
+}
+
+void fadeInGameOverScreen(sf::RenderWindow& window, sf::Font& font) {
+    if (gameOverScreenAlpha < 255.0f) {
+        gameOverScreenAlpha += fadeSpeed * sf::Time(sf::seconds(1)).asSeconds(); // Fix for frame time
+        if (gameOverScreenAlpha > 255.0f) {
+            gameOverScreenAlpha = 255.0f;
+        }
+    }
+
+    sf::RectangleShape fadeRect(sf::Vector2f(window.getSize().x, window.getSize().y));
+    fadeRect.setFillColor(sf::Color(0, 0, 0, static_cast<sf::Uint8>(gameOverScreenAlpha)));
+    window.draw(fadeRect);
+
+    sf::Text gameOverText;
+    gameOverText.setFont(font); // Use the provided font
+    gameOverText.setString("Game Over! Press Enter to restart.");
+    gameOverText.setCharacterSize(20);
+    gameOverText.setFillColor(sf::Color::Red);
+    gameOverText.setPosition(window.getSize().x / 2 - gameOverText.getGlobalBounds().width / 2, window.getSize().y / 2 - 20);
+    window.draw(gameOverText);
+}
+
+// ... (Previous code remains unchanged)
+
 int main() {
-    // Create a window
     sf::RenderWindow window(sf::VideoMode(gridSize * cellSize, gridSize * cellSize), "SFML Snake Game");
 
-    // Create the snake and food with an initial length of 3
     Snake snake(3);
     Food food;
-
-    // Set the initial direction to the right
     snake.setDirection(Direction::Right);
 
-    // Create a clock for controlling the frame rate
     sf::Clock clock;
+    sf::Clock gameOverClock; // Clock for fade-in effect
 
-    // Main game loop
+    Menu menu(window);
+
+    bool inMenu = true;
+    bool inGameOverScreen = false;
+
     while (window.isOpen()) {
-        // Handle events
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            // Handle keyboard input to change the snake's direction
             if (event.type == sf::Event::KeyPressed) {
-                switch (event.key.code) {
-                    case sf::Keyboard::Up:
-                        snake.setDirection(Direction::Up);
-                        break;
-                    case sf::Keyboard::Down:
-                        snake.setDirection(Direction::Down);
-                        break;
-                    case sf::Keyboard::Left:
-                        snake.setDirection(Direction::Left);
-                        break;
-                    case sf::Keyboard::Right:
-                        snake.setDirection(Direction::Right);
-                        break;
+                if (inGame) {
+                    switch (event.key.code) {
+                        case sf::Keyboard::Up:
+                            snake.setDirection(Direction::Up);
+                            break;
+                        case sf::Keyboard::Down:
+                            snake.setDirection(Direction::Down);
+                            break;
+                        case sf::Keyboard::Left:
+                            snake.setDirection(Direction::Left);
+                            break;
+                        case sf::Keyboard::Right:
+                            snake.setDirection(Direction::Right);
+                            break;
+                    }
+                } else if (inGameOverScreen && event.key.code == sf::Keyboard::Enter) {
+                    inGameOverScreen = false;
+                    inMenu = true;
+                    gameOverClock.restart();
+                } else {
+                    menu.update(event);
                 }
             }
         }
 
-        // Move the snake every 0.1 seconds
-        if (clock.getElapsedTime().asSeconds() >= 0.1) {
-            // Move the snake
-            snake.move();
+        if (inMenu) {
+            window.clear();
+            menu.draw();
 
-            // Check for collisions with itself or the window boundaries
-            if (snake.checkCollision()) {
-                std::cout << "Game Over! Collision with the window boundary or self.\n";
+            if (menu.getSelectedItemIndex() == 0 && sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
+                inGame = true;
+                inMenu = false;
+                snake = Snake(3); // Reset the snake
+                food.respawn();   // Respawn the food
+            } else if (menu.getSelectedItemIndex() == 1 && sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
                 window.close();
             }
 
-            // Check for collisions with food
-            if (snake.getHeadPosition() == food.getPosition()) {
-                // If the snake eats the food, grow and respawn the food
-                snake.grow();
-                food.respawn();
-            }
-
-            clock.restart();
+            window.display();
+            continue;
         }
 
-        // Clear the window
+        if (!inGameOverScreen) {
+            if (clock.getElapsedTime().asSeconds() >= 0.1) {
+                snake.move();
+
+                if (snake.checkCollision()) {
+                    inGameOverScreen = true;
+                    gameOverClock.restart();
+                }
+
+                if (snake.getHeadPosition() == food.getPosition()) {
+                    snake.grow();
+                    food.respawn();
+                }
+
+                clock.restart();
+            }
+        }
+
         window.clear();
 
-        // Draw the food and snake
-        food.draw(window);
-        snake.draw(window);
+        if (inGameOverScreen) {
+            fadeInGameOverScreen(window, menu.getFont());
 
-        // Display the contents of the window
+            // Add a delay after game over for the fade-in effect
+            if (gameOverClock.getElapsedTime().asSeconds() >= 1.0) {
+                inGameOverScreen = false;
+                inMenu = true;
+            }
+        } else {
+            food.draw(window);
+            snake.draw(window);
+        }
+
         window.display();
     }
 
